a632b75c41a76c281f982e4af2ae54aaa9e26db4
Egormity <kotlaregor9@gmail.com>
2024-11-28 17:04:20 +0700

    justom some styling

--- 002-guess-number/App.js
+++ 002-guess-number/App.js
@@ -1,8 +1,27 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
 
 export default function App() {
-    return <StartGameScreen />;
+    return (
+        <LinearGradient
+            style={styles.container}
+            colors={["#4E0329", "#DDb52F"]}
+        >
+            <ImageBackground
+                source={require("./assets/images/background.png")}
+                resizeMode="cover"
+                style={styles.container}
+                imageStyle={{ opacity: 0.15 }}
+            >
+                <StartGameScreen />
+            </ImageBackground>
+        </LinearGradient>
+    );
 }
